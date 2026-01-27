be2168ba654dd406ab8ab7085cdca0bf07354c4b
NikhithaGone <nikhithagone304@gmail.com>
2026-01-27 23:42:56 +0530

    Initial commit: Gradle Java project with JaCoCo

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    application
+    java
+    jacoco
+}
+
+group = "com.bits"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+}
+
+application {
+    mainClass.set("com.bits.mvn.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(false)
+    }
+}
